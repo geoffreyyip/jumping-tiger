f509ae8a4db4627e7b54e12ab64c9f5b256dba2b
imagiraffe123 <gyip4@binghamton.edu>
2017-01-09 17:54:51 -0500

    Update stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-const webpack = require('webpack');
 const path = require('path');
 
 /**
@@ -8,28 +7,36 @@ const path = require('path');
  * they're usually the same, but not always
  * they can differ if called from command line
  */
-var DEV = path.resolve(__dirname, 'src');
-var OUTPUT = path.resolve(__dirname, 'output');
+const DEV = path.resolve(__dirname, 'src');
+const OUTPUT = path.resolve(__dirname, 'output');
 
 /**
- * entry and output are required properties
- * entry tells webpack where to start its graph of dependencies
- * output tells webpack where to emit its bundle
- * module delegates per-file transformations to outside libraries
- * plugins act on "chunks" of files; uglify included by default
+ * entry, output, module, and plugins are core webpack concepts
+ * entry and output are required properties, entry tells webpack
+ * where to start its graph of dependencies, and output tells webpack
+ * where to emit its bundle
+ *
+ * module tells webpack what to do with individual files; usu. by
+ * delegating to outside loaders
+ *
+ * plugins tells webpack how to transform chunks of files; uglify is
+ * included by default and it deletes unused js code
+ *
+ * eslint property is specified for eslint-loader, it also watches for
+ * changes during webpack-dev-server
  */
-var config = {
-  entry: DEV + "/index.jsx",
+const config = {
+  entry: `${DEV}/index.jsx`,
   output: {
     path: OUTPUT,
-    filename: "myCode.js"
+    filename: 'myCode.js',
   },
   module: {
     rules: [
       { test: /\.(js|jsx)$/, use: 'babel-loader' },
       { test: /\.(js|jsx)$/, use: 'eslint-loader', enforce: 'pre' },
-    ]
-  }
+    ],
+  },
 };
 
 module.exports = config;
